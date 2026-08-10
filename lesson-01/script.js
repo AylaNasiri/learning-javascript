3ee2f74effce5817529171b750181c1ae087334c
AylaNasiri <ayla.nasiri.dev@gmail.com>
2026-08-10 13:38:15 +0330

    Lesson 01: JavaScript Basics

---
+++ lesson-01/script.js
@@ -0,0 +1,23 @@
+// This is my first JavaScript code
+
+/*
+Day 1
+Learning JavaScript
+*/ 
+
+//"use strict";
+
+console.log("====================");
+console.log("Hello Everyone");
+console.log("Course Name: Learning JavaScript");
+console.log("Lesson: 01");
+console.log("Status: Started");
+console.log("The sum of 10 + 20 is: ");
+console.log(10 + 20);
+console.log("====================");
+
+alert("Welcome to JavaScript!");
+
+
+
+
