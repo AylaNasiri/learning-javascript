e6b270059e1ffdf5ee114afd69ba677a38cb710c
AylaNasiri <ayla.nasiri.dev@gmail.com>
2026-08-20 15:47:03 +0330

    Complete Lesson 02: Variables and Data Types

---
+++ lesson-02/script.js
@@ -0,0 +1,19 @@
+
+"use strict";
+
+const firstName = "Ayla";
+const course = "JavaScript";
+let currentDay = 2;
+const isLearning = true;
+let completedLessons = 2;
+
+console.log("====================");
+console.log("Student Profile");
+console.log("Name:", firstName);
+console.log("Course:", course);
+console.log("Day:", currentDay);
+console.log("Learning:", isLearning);
+console.log("Completed Lessons:", completedLessons);
+console.log("====================");
+
+
